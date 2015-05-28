f70c30c869f49145f78dded0f81469a4ea87ebc5
Christopher Davis <cdavis9999@gmail.com>
2015-05-28 12:02:21 -0400

    Make the Plugin Composer Friendly

--- google-tag-manager.php
+++ google-tag-manager.php
@@ -6,30 +6,32 @@
  * Version: 1.0
  * Text Domain: pmg-google-tag-manager
  * Author: Christopher Davis
- * Author URI: http://pmg.co/people/chris
- * License: GPL-2.0+
+ * Author URI: https://www.pmg.com
+ * License: MIT
  *
- * Copyright 2013 Performance Media Group <http://pmg.co>
+ * Copyright (c) 2015 PMG <https://www.pmg.com>
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License, version 2, as 
- * published by the Free Software Foundation.
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
  *
  * @category    WordPress
- * @package     GoogleTagManager
- * @since       1.0
- * @author      Christopher Davis <chris@pmg.co>
- * @copyright   2013 Performance Media Group
- * @license     http://opensource.org/licenses/GPL-2.0 GPL-2.0+
+ * @author      Christopher Davis <http://christopherdavis.me>
+ * @license     http://opensource.org/licenses/MIT MIT
  */
 
 namespace PMG\GoogleTagManager;
